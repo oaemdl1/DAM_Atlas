b527375d8b726b7ad29a0952e6b06e3c34c42fc3
Luis Reque <luis.reque86@gmail.com>
2015-07-18 02:41:53 +0000

    Home controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718012809) do
+ActiveRecord::Schema.define(version: 20150718014557) do
 
-  create_table "businesses", force: :cascade do |t|
-    t.text     "razonsocial",        limit: 65535
-    t.string   "ruc",                limit: 255
-    t.string   "telefonofijo",       limit: 255
-    t.string   "telefonomovil",      limit: 255
-    t.string   "partidaregistral",   limit: 255
-    t.string   "representantelegal", limit: 255
-    t.string   "correo",             limit: 255
-    t.string   "direccion",          limit: 255
-    t.string   "linkgooglemaps",     limit: 255
-    t.integer  "eliminado",          limit: 4
-    t.integer  "districts_id",       limit: 4
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+  create_table "blocks", force: :cascade do |t|
+    t.integer  "specialty_id",    limit: 4
+    t.string   "semestre",        limit: 255
+    t.string   "ciclo_academico", limit: 255
+    t.string   "bloque",          limit: 255
+    t.string   "dia_semana",      limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  add_index "businesses", ["districts_id"], name: "index_businesses_on_districts_id", using: :btree
+  add_index "blocks", ["specialty_id"], name: "index_blocks_on_specialty_id", using: :btree
 
   create_table "districts", force: :cascade do |t|
     t.string   "detalle",    limit: 255
@@ -37,4 +31,23 @@ ActiveRecord::Schema.define(version: 20150718012809) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "positions", force: :cascade do |t|
+    t.string   "detalle",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "reasons", force: :cascade do |t|
+    t.string   "detalle",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string   "detalle",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_foreign_key "blocks", "specialties"
 end
