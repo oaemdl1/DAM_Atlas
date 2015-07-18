c856fd80f2e5fac77596687dc4578c160b98cb0c
omar delacruz <delacruz.sd@gmail.com>
2015-07-18 03:46:39 +0000

    merge

--- app/models/pea.rb
+++ app/models/pea.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: peas
+#
+#  id           :integer          not null, primary key
+#  specialty_id :integer
+#  ciclo        :string(255)
+#  detalle      :text(65535)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Pea < ActiveRecord::Base
   belongs_to :specialty
 end
--- test/models/pea_avance_test.rb
+++ test/models/pea_avance_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: pea_avances
+#
+#  id             :integer          not null, primary key
+#  pea_id         :integer
+#  dominio        :string(255)
+#  oper_ejecutar  :boolean
+#  oper_seminario :boolean
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'test_helper'
 
 class PeaAvanceTest < ActiveSupport::TestCase
